a6e3922995a5fdd6554f80c29b563229ef770a5a
Volkan Ünlü <36553214+volkanunlu@users.noreply.github.com>
2022-01-13 17:34:58 +0300

    Add files via upload

---
+++ Array_repeat_2k/src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+
+    static boolean isFind(int[]arr,int value)
+    {
+        for (int i:arr)
+        {
+            if (i==value)
+            {
+                return true;
+            }
+
+        }
+        return false;
+
+    }
+
+
+    public static void main(String[] args) {
+    int [] list={24,41,19,24,12,8,12,9,6,14,25,8};
+    int [] duplicate= new int[list.length];
+    int startIndex=0;
+    for(int i=0;i<list.length;i++)
+    {
+        for (int j=0;j<list.length;j++)
+        {
+            if((i!=j)&&(list[i]==list[j])&&(list[i]%2==0))
+            {
+                if (!isFind(duplicate,list[i]))
+                {
+                    duplicate[startIndex++]=list[i];
+                }
+                break;
+            }
+        }
+    }
+    for (int value:duplicate){
+
+        if (value!=0)
+        {
+            System.out.println(value);
+        }
+
+    }
+
+
+    }
+}
